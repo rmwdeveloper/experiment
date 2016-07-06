6a52433f7b5b7ecda33f38f89ef753aafc4769c5
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-06 17:20:11 -0500

    Update home

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -4,10 +4,10 @@ import React, { PropTypes } from 'react';
 const title = 'React Starter Kit';
 
 function Home({ news }, context) {
-    context.setTitle(title);
-    return (
-        <div> Hello World</div>
-    );
+  context.setTitle(title);
+  return (
+    <div> Hello World</div>
+  );
 }
 
 Home.contextTypes = { setTitle: PropTypes.func.isRequired };
