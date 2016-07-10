101379df04a716b787a50f8a89b465df3121dc9d
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-10 18:27:14 -0500

    cleaner classnames

--- src/components/ConsoleInput/ConsoleInput.js
+++ src/components/ConsoleInput/ConsoleInput.js
@@ -1,10 +1,10 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './ConsoleInput.css';
-
+import cx from 'classNames';
 
 function ConsoleInput(props) {
-  return <input className={`${styles.root} ${props.className}`} type="text" />;
+  return <input className={cx(styles.root, props.className)} type="text" />;
 }
 
 export default withStyles(styles)(ConsoleInput);
--- src/routes/console/Console.js
+++ src/routes/console/Console.js
@@ -1,14 +1,15 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Console.css';
+import cx from 'classNames';
 import ConsoleInput from '../../components/ConsoleInput';
 
 const title = 'Console';
 function Console(props, context) {
   context.setTitle(title);
   return (
-    <div className={styles.root}>
-      <ConsoleInput className={styles.consoleInput} />
+    <div className={cx(styles.root, 'row bottom-lg')}>
+      <ConsoleInput className={cx(styles.consoleInput, 'col-lg-12')} />
     </div>
   );
 }
