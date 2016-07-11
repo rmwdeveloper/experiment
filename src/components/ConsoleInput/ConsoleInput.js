587bdde4964d333137ea28c44dda971f7fc7f01d
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-11 14:06:37 -0500

    Edit some styles

--- src/components/ConsoleInput/ConsoleInput.js
+++ src/components/ConsoleInput/ConsoleInput.js
@@ -1,10 +1,14 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './ConsoleInput.css';
-import cx from 'classNames';
+import cx from 'classnames';
 
 function ConsoleInput(props) {
-  return <input className={cx(styles.root, props.className)} type="text" />;
+  return (
+    <div className={cx(styles.root, props.className)}>
+      <input type="text" />
+    </div>
+  );
 }
 
 export default withStyles(styles)(ConsoleInput);
