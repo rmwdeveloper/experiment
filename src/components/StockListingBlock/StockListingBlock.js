7896207e715ccb0659162b6f2af5708ba45cf3d2
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-12 18:34:19 -0500

    Edit styling, start stock listing block

---
+++ src/components/StockListingBlock/StockListingBlock.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './StockListingBlock.css'; //eslint-disable-line
+import cx from 'classnames';
+
+function StockListingBlock() {
+  return (
+      <div>
+        
+      </div>
+  );
+}
+
+export default withStyles(styles)(StockListingBlock);
