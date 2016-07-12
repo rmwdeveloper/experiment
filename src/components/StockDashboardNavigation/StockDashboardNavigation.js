bce3c3b35688e2954a99295fb7b2495c9da14acd
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-12 14:12:24 -0500

    DAshboard styling

--- src/components/StockDashboardNavigation/StockDashboardNavigation.js
+++ src/components/StockDashboardNavigation/StockDashboardNavigation.js
@@ -2,17 +2,32 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './StockDashboardNavigation.css'; //eslint-disable-line
 import Link from '../Link';
-
+import cx from 'classnames';
 
 function StockDashboardNavigation() {
   return (
     <header className={`${styles.root}  row center-xs`} role="navigation">
       <div className={`${styles.linkContainer} col-xs`}>
-        <Link className={styles.link} to="/stocks/search"><i className="fa fa-2x fa-search" /></Link>
-        <Link className={styles.link} to="/stocks/explore"><i className="fa fa-2x fa-compass" /></Link>
-        <Link className={styles.link} to="/stocks/pick"><i className="fa fa-2x fa-industry" /></Link>
-        <Link className={styles.link} to="/stocks/watchlist"><i className="fa fa-2x fa-eye" /></Link>
-        <Link className={styles.link} to="/stocks/alerts"><i className="fa fa-2x fa-exclamation" /></Link>
+        <Link className={styles.link} to="/stocks/search">
+          <span className={styles.desktop}>Search</span>
+          <i className={cx('fa fa-2x fa-search', styles.mobile)} />
+        </Link>
+        <Link className={styles.link} to="/stocks/explore">
+          <span className={styles.desktop}>Explore</span>
+          <i className={cx('fa fa-2x fa-compass', styles.mobile)} />
+        </Link>
+        <Link className={styles.link} to="/stocks/pick">
+          <span className={styles.desktop}>Pick</span>
+          <i className={cx('fa fa-2x fa-industry', styles.mobile)} />
+        </Link>
+        <Link className={styles.link} to="/stocks/watchlist">
+          <span className={styles.desktop}>Watch</span>
+          <i className={cx('fa fa-2x fa-eye', styles.mobile)} />
+        </Link>
+        <Link className={styles.link} to="/stocks/alerts">
+          <span className={styles.desktop}>Alerts</span>
+          <i className={cx('fa fa-2x fa-exclamation', styles.mobile)} />
+        </Link>
       </div>
     </header>
   );
