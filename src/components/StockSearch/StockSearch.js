df0735a394b6a842999099aa922a98db36f8caf1
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-12 18:16:17 -0500

    Search

--- src/components/StockSearch/StockSearch.js
+++ src/components/StockSearch/StockSearch.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './StockSearch.css'; //eslint-disable-line
-
+import cx from 'classnames';
 
 function StockSearch() {
   return (
-    <div>Stock Search</div>
+      <div className={cx(styles.root, 'row center-lg center-md center-sm center-xs')}>
+        <div className="col-lg">
+          <input type="search" placeholder="Search By Company Name or Ticker" />
+        </div>
+      </div>
   );
 }
 
