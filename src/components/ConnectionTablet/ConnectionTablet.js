72eada3df3a2106ffaba2ca0464a7437f5258623
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-18 18:19:49 -0500

    Add connectiontab

---
+++ src/components/ConnectionTablet/ConnectionTablet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './ConnectionsTablet.css'; //eslint-disable-line
+import cx from 'classnames';
+
+function ConnectionsTablet() {
+  return (
+    <div>
+      connections
+    </div>
+  );
+}
+
+export default withStyles(styles)(ConnectionsTablet);
