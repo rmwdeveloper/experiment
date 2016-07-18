94701fa98f446226919fd23853a4dd79d6c63302
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-18 15:49:28 -0500

    Layout Cell

--- src/components/LayoutCell/LayoutCell.js
+++ src/components/LayoutCell/LayoutCell.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './LayoutCell.css'; //eslint-disable-line
 import cx from 'classnames';
 
-function LayoutCell() {
+function LayoutCell({ children }) {
   return (
-      <div>
-        Layout Cell
-      </div>
+    <div>
+      {this.props.children}
+    </div>
   );
 }
 
+LayoutCell.propTypes = {
+  children: PropTypes.element
+};
 export default LayoutCell;
