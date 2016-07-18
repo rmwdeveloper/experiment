8536d365a715cc82018e80f836f64bd3f43b3b2b
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-18 14:34:59 -0500

    component registrty

--- src/components/widgetRegistry.js
+++ src/components/widgetRegistry.js
@@ -1,10 +1,15 @@
+import UserBlock from './UserBlock/UserBlock';
+import Ratings from './Ratings/Ratings';
+import PostEditor from './PostEditor/PostEditor';
+import Watchlist from './Watchlist/Watchlist';
+
+
 const widgetRegistry = {};
 
-widgetRegistry.Featurette = Featurette;
-widgetRegistry.Carousel = Carousel;
-widgetRegistry.CoursesRow = CoursesRow;
-widgetRegistry.HeadingIconsRow = HeadingIconsRow;
-widgetRegistry.TestimonialsRow = TestimonialsRow;
+widgetRegistry.userblock = UserBlock;
+widgetRegistry.ratings = Ratings;
+widgetRegistry.posteditor = PostEditor;
+widgetRegistry.watchlist = Watchlist;
 
 
 export default widgetRegistry;
