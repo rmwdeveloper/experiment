2ed0da657af509ebbfad30e19e403ecea4874650
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-18 18:28:18 -0500

    All styles in place for connection and personalinfo

--- src/components/ConnectionTablet/ConnectionTablet.js
+++ src/components/ConnectionTablet/ConnectionTablet.js
@@ -3,10 +3,15 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './ConnectionsTablet.css'; //eslint-disable-line
 import cx from 'classnames';
 
-function ConnectionsTablet() {
+function ConnectionsTablet({title, amount}) {
   return (
-    <div>
-      connections
+    <div className={styles.root}>
+      <div className={styles.title}>
+        <a href="#">{title}</a>
+      </div>
+      <div className={styles.amount}>
+        <a href="#">{amount}</a>
+      </div>
     </div>
   );
 }
--- src/components/ConnectionsInfo/ConnectionsInfo.js
+++ src/components/ConnectionsInfo/ConnectionsInfo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './ConnectionsInfo.css'; //eslint-disable-line
-import cx from 'classnames';
+import ConnectionTablet from '../ConnectionTablet/ConnectionTablet';
 
 function ConnectionsInfo() {
   return (
     <div>
-      connections
+      <ConnectionTablet title="Feeds" amount={0} />
+      <ConnectionTablet title="Followers" amount={0} />
+      <ConnectionTablet title="Following" amount={1} />
+      <ConnectionTablet title="Rank" amount={24} />
     </div>
   );
 }
