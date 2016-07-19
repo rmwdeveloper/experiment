3bf98d095b69b15a7ff62629fd42f10fe8fa642b
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-19 13:35:28 -0500

    start search dropdown

---
+++ src/components/SearchDropdown/SearchDropdown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './SearchDropdown.css'; //eslint-disable-line
+
+
+function SearchDropdown() {
+  return (
+    <div>SearchDropdown</div>
+  );
+}
+
+export default withStyles(styles)(SearchDropdown);
