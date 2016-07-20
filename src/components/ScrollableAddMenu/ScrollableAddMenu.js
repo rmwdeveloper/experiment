ea90331842cf5ac26b8d690a54ad9e87d1be3d96
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-20 16:20:26 -0500

    list all widgets in ..

--- src/components/ScrollableAddMenu/ScrollableAddMenu.js
+++ src/components/ScrollableAddMenu/ScrollableAddMenu.js
@@ -2,11 +2,16 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './ScrollableAddMenu.css'; //eslint-disable-line
 import cx from 'classnames';
-
+import widgetRegistry from '../widgetRegistry';
 function ScrollableAddMenu({columnHeight}) {
+  console.log(Object.keys(widgetRegistry));
   return (
       <div  style={{minHeight: `${ columnHeight / 2}rem` }} className={styles.root}>
-        scrollableaddmenu
+        {
+          Object.keys(widgetRegistry).map(widgetType => {
+            return <span className="addBlock">Make this block a {widgetType}</span>;
+          })
+        }
       </div>
   );
 }
