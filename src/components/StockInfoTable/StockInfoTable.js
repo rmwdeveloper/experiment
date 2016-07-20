01bb3dca84e4b8f1cccc8f9a87fe767fb031d116
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-20 18:25:05 -0500

    Style

--- src/components/StockInfoTable/StockInfoTable.js
+++ src/components/StockInfoTable/StockInfoTable.js
@@ -2,9 +2,9 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './StockInfoTable.css'; //eslint-disable-line
 
-
+// todo: Refactor return statement
 function StockInfoTable({quotes}) {
-  console.log(quotes);
+
   return (
     <table className={styles.root}>
       <colgroup span="4"></colgroup>
@@ -17,8 +17,8 @@ function StockInfoTable({quotes}) {
         <th>MSDate</th>
         <th>Market Cap</th>
         <th>Volume</th>
-        <th>ChangeYTD</th>
-        <th>ChangePercentYTD</th>
+        <th>Change YTD</th>
+        <th>Change Percent YTD</th>
         <th>High</th>
         <th>Low</th>
         <th>Open</th>
@@ -29,13 +29,13 @@ function StockInfoTable({quotes}) {
             <td>{quotes[quote].Name}</td>
             <td>{quotes[quote].Symbol}</td>
             <td>{quotes[quote].LastPrice}</td>
-            <td>{quotes[quote].Change}</td>
-            <td>{quotes[quote].ChangePercent}</td>
-            <td>{quotes[quote].MSDate}</td>
+            <td style={{ color: parseFloat(quotes[quote].Change).toPrecision(2) > 0 ? 'green' : 'red' }}>{parseFloat(quotes[quote].Change).toPrecision(2)}</td>
+            <td style={{ color: parseFloat(quotes[quote].ChangePercent).toPrecision(2) > 0 ? 'green' : 'red' }} >{parseFloat(quotes[quote].ChangePercent).toPrecision(2)}</td>
+            <td>{parseFloat(quotes[quote].MSDate).toPrecision(7)}</td>
             <td>{quotes[quote].MarketCap}</td>
             <td>{quotes[quote].Volume}</td>
             <td>{quotes[quote].ChangeYTD}</td>
-            <td>{quotes[quote].ChangePercentYTD}</td>
+            <td style={{ color: parseFloat(quotes[quote].ChangePercentYTD).toPrecision(2) > 0 ? 'green' : 'red' }}>{parseFloat(quotes[quote].ChangePercentYTD).toPrecision(3)}</td>
             <td>{quotes[quote].High}</td>
             <td>{quotes[quote].Low}</td>
             <td>{quotes[quote].Open}</td>
