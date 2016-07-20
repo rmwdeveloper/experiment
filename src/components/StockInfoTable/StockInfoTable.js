524308ab0fda1fb1a437f84f2c4a6677bb98ba01
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-20 18:14:17 -0500

    stock info table

--- src/components/StockInfoTable/StockInfoTable.js
+++ src/components/StockInfoTable/StockInfoTable.js
@@ -4,8 +4,45 @@ import styles from './StockInfoTable.css'; //eslint-disable-line
 
 
 function StockInfoTable({quotes}) {
+  console.log(quotes);
   return (
-    <div>Stock Info Table</div>
+    <table className={styles.root}>
+      <colgroup span="4"></colgroup>
+      <tr>
+        <th>Name</th>
+        <th>Symbol</th>
+        <th>Price</th>
+        <th>Change</th>
+        <th>Change Percent</th>
+        <th>MSDate</th>
+        <th>Market Cap</th>
+        <th>Volume</th>
+        <th>ChangeYTD</th>
+        <th>ChangePercentYTD</th>
+        <th>High</th>
+        <th>Low</th>
+        <th>Open</th>
+      </tr>
+      {
+        Object.keys(quotes).map(quote => {
+          return (<tr>
+            <td>{quotes[quote].Name}</td>
+            <td>{quotes[quote].Symbol}</td>
+            <td>{quotes[quote].LastPrice}</td>
+            <td>{quotes[quote].Change}</td>
+            <td>{quotes[quote].ChangePercent}</td>
+            <td>{quotes[quote].MSDate}</td>
+            <td>{quotes[quote].MarketCap}</td>
+            <td>{quotes[quote].Volume}</td>
+            <td>{quotes[quote].ChangeYTD}</td>
+            <td>{quotes[quote].ChangePercentYTD}</td>
+            <td>{quotes[quote].High}</td>
+            <td>{quotes[quote].Low}</td>
+            <td>{quotes[quote].Open}</td>
+          </tr>);
+        })
+      }
+    </table>
   );
 }
 
