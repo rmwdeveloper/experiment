73939d0c5a963fbabdc5c4784d433a2af75d1047
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-20 18:32:26 -0500

    start stockchart

---
+++ src/components/StockChart/StockChart.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './StockChart.css'; //eslint-disable-line
+
+
+function StockChart() {
+  return (
+    <div className={styles.root}>
+
+    </div>
+  );
+}
+
+
+export default withStyles(styles)(StockChart);
