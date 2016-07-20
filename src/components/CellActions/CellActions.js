d82cff9f55dcd2917a9be5f76a323be571c6b4d1
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-20 15:56:15 -0500

    LAYOUT_ENGINE

--- src/components/CellActions/CellActions.js
+++ src/components/CellActions/CellActions.js
@@ -2,13 +2,14 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './CellActions.css'; //eslint-disable-line
 import cx from 'classnames';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import ScrollableAddMenu from '../ScrollableAddMenu';
 function CellActions({rowWidth, columnHeight, toggleEditCellMode, cellIndex, editing}) {
   return (
     <div className={styles.root}>
       {
-        editing ? <ScrollableAddMenu /> :
+        editing ? <ScrollableAddMenu key={cellIndex} /> :
           <i onClick={() => {toggleEditCellMode(cellIndex)}}
              style={{ fontSize: `${rowWidth * (columnHeight * 2) / 100}rem`}}
              className={cx(styles.addWidget, "fa fa-plus")}/>
