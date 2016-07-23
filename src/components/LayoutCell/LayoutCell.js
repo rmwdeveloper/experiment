78b9ea10e5f2aeae063ba4b3172e45445ef219f1
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-23 14:29:00 -0500

    Bototm L:eft and Rgiht

--- src/components/LayoutCell/LayoutCell.js
+++ src/components/LayoutCell/LayoutCell.js
@@ -26,8 +26,13 @@ class LayoutCell extends Component {
       initialClickPageX: null,
       initialClickCellWidth: null,
       width: null,
+      initialClickPageY: null,
+      initialClickCellHeight: null,
+      height: null,
+      minHeight: null,
       transform: null
     };
+    this.initialMinHeight = 100;
     this.resize = this.resize.bind(this);
     this.startResize = this.startResize.bind(this);
     this.endResize = this.endResize.bind(this);
@@ -37,33 +42,58 @@ class LayoutCell extends Component {
   resize(event) {
     event.preventDefault();
     const { pageX, pageY } = event;
-    const { cornerClicked, initialClickPageX, initialClickCellWidth } = this.state;
+    const {
+      cornerClicked, initialClickPageX, initialClickCellWidth, initialClickPageY,
+      initialClickCellHeight
+    } = this.state;
     const xDirection = pageX < initialClickPageX ? 'left' : 'right';
+    const yDirection = pageY < initialClickPageY ? 'up' : 'down';
     const movedX = pageX - initialClickPageX;
+    const movedY = pageY - initialClickPageY;
+
     let transform = null;
     let width = null;
-    if ((movedX > initialClickCellWidth)) {
-      return null;
-    }
+    let height = null;
+    let minHeight = null;
+
     switch (cornerClicked) {
       case 'bottomRight':
         width = xDirection === 'right' ? `${initialClickCellWidth + movedX}px` : `${initialClickCellWidth + movedX}px`;
+        height = `${initialClickCellHeight + movedY}px`;
+        minHeight = `${initialClickCellHeight + movedY}px`;
         break;
       case 'bottomLeft':
+        if ((movedX > initialClickCellWidth) && xDirection === 'right') {
+          return null;
+        }
         width = xDirection === 'right' ? `${initialClickCellWidth - movedX}px` : `${initialClickCellWidth + Math.abs(movedX)}px`;
+        height = `${initialClickCellHeight + movedY}px`;
+        minHeight = `${initialClickCellHeight + movedY}px`;
         transform = `translateX(${movedX}px)`;
         break;
       case 'topRight':
+        if ((pageY - 100) < 0 || yDirection === 'down') {
+          return null;
+        }
+        transform = `translateY(${movedY}px)`;
+        height = `${initialClickCellHeight + Math.abs(movedY)}px`;
+        minHeight = `${initialClickCellHeight + Math.abs(movedY)}px`;
         width = xDirection === 'right' ? `${initialClickCellWidth + movedX}px` : `${initialClickCellWidth + movedX}px`;
         break;
       case 'topLeft':
+
+        if ((pageY - 100) < 0 || yDirection === 'down') {
+          return null;
+        }
+        transform = `translateY(${movedY}px) translateX(${movedX}px)`;
+        height = `${initialClickCellHeight + Math.abs(movedY)}px`;
+        minHeight = `${initialClickCellHeight + Math.abs(movedY)}px`;
         width = xDirection === 'right' ? `${initialClickCellWidth - movedX}px` : `${initialClickCellWidth + Math.abs(movedX)}px`;
-        transform = `translateX(${movedX}px)`;
         break;
       default:
         break;
     }
-  this.setState({width, transform});
+    this.setState({ width, transform, height, minHeight });
   }
 
   startResize(event, cornerClicked) {
@@ -89,6 +119,8 @@ class LayoutCell extends Component {
       resizing: true,
       initialClickPageX: event.pageX,
       initialClickCellWidth: this.widgetCell.offsetWidth,
+      initialClickPageY: event.pageY,
+      initialClickCellHeight: this.widgetCell.offsetHeight,
       cornerClicked,
     });
     this.mouseUp = window.addEventListener('mouseup', this.endResize);
@@ -96,29 +128,38 @@ class LayoutCell extends Component {
   }
 
   endResize() {
-    this.setState({resizing: false});
-    // this.setState({ resizing: false, pageX: this.state.resizeStartX, pageY: this.state.resizeStartY });
-    // this.setState({ resizing: false, pageX: this.state.resizeStartX });
+    this.setState({
+      resizing: false,
+      initialClickPageX: null,
+      initialClickCellWidth: null,
+      initialClickPageY: null,
+      initialClickCellHeight: null,
+      cornerClicked: null,
+    });
     window.removeEventListener('mouseup', this.endResize);
     window.removeEventListener('mousemove', this.resize);
   }
+
   componentDidMount() {
     this.dashBoard = document.getElementById('stockDashboard');
     this.widgetCell = document.getElementById(`cell${this.props.layoutIndices[0]}`);
     this.widgetWidth = this.widgetCell.offsetWidth;
     this.widgetRect = this.widgetCell.getBoundingClientRect();
   }
+
   componentDidUpdate() {
     this.widgetCell = document.getElementById(`cell${this.props.layoutIndices[0]}`);
   }
+
   render() {
     const { mode, gridVisible, columnHeight, widget, rowWidth, layoutIndices, addStockWidget, toggleEditCellMode, cellIndex, editing } = this.props;
-    const { resizing, pageX, pageY, resizeStartX, initialOffsetX, transform, width, height } = this.state;
+    const { resizing, transform, width, height, minHeight } = this.state;
     const border = gridVisible ? '1px dashed black' : 'medium none';
     let visibility = mode === 'preview' ? 'hidden' : 'visible';
     const style = { border };
     if (resizing) {
       style.height = height;
+      style.minHeight = minHeight;
       style.width = width;
       style.transform = transform;
     }
@@ -149,13 +190,17 @@ class LayoutCell extends Component {
           }
 
         </div>
-        <span style={{cursor: 'nwse-resize', bottom: '-5px', right: '-5px'}} onMouseDown={(event) => { this.startResize(event, 'bottomRight'); }}
+        <span style={{cursor: 'nwse-resize', bottom: '-5px', right: '-5px'}}
+              onMouseDown={(event) => { this.startResize(event, 'bottomRight'); }}
               className={styles.resizeHandle}> </span>
-          <span style={{cursor: 'nesw-resize', bottom: '-5px', left: '-5px'}} onMouseDown={(event) => { this.startResize(event, 'bottomLeft'); }}
+          <span style={{cursor: 'nesw-resize', bottom: '-5px', left: '-5px'}}
+                onMouseDown={(event) => { this.startResize(event, 'bottomLeft'); }}
                 className={styles.resizeHandle}> </span>
-          <span style={{cursor: 'nesw-resize', top: '-5px', right: '-5px'}} onMouseDown={(event) => { this.startResize(event, 'topRight'); }}
+          <span style={{cursor: 'nesw-resize', top: '-5px', right: '-5px'}}
+                onMouseDown={(event) => { this.startResize(event, 'topRight'); }}
                 className={styles.resizeHandle}> </span>
-          <span style={{cursor: 'nwse-resize', top: '-5px', left: '-5px'}} onMouseDown={(event) => { this.startResize(event, 'topLeft'); }}
+          <span style={{cursor: 'nwse-resize', top: '-5px', left: '-5px'}}
+                onMouseDown={(event) => { this.startResize(event, 'topLeft'); }}
                 className={styles.resizeHandle}> </span>
       </div>
     );
