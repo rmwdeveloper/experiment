9bb97ae15ae6a6ac3a740d64f9e083ec0f2ef060
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-23 18:40:03 -0500

    slightly edit modal style

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,14 +3,11 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Modal.css'; //eslint-disable-line
 
 
-function Modal({ id, modalContent, modalVisible, modalTitle, closeModal }) {
+function Modal({ id, modalContent, modalVisible, closeModal }) {
   const display = modalVisible ? 'block' : 'none';
   return (
     <div id={id} style={{display}} className={styles.root}>
       <div className={styles.modalInner}>
-        <div className={styles.modalHeader}>
-          {modalTitle}
-        </div>
         <div className={styles.modalBody}>
           {modalContent}
         </div>
