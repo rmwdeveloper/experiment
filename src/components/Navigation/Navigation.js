6424521d074b6f9f38fc59db022ea53d53f96484
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-29 18:03:25 -0500

    Temporarily remove stock dashboard link. Start windows. Cust om styles for dev only link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Navigation.css'; //eslint-disable-line
 import Link from '../Link';
 import ReactPerfButton  from '../ReactPerfButton';
-
+import cx from 'classnames';
 
 function Navigation() {
   return (
@@ -11,8 +11,10 @@ function Navigation() {
       <div className={`${styles.linkContainer} col-lg-12 around-lg`}>
         <Link className={styles.link} to="/"> Robert Westenberger </Link>
         <Link className={styles.link} to="/console">Console</Link>
-        <Link className={styles.link} to="/stocks">Stock Dashboard</Link>
-        {__DEV__ ? <ReactPerfButton className={styles.link} /> : null}
+        <Link className={styles.link} to="/xp">Windows</Link>
+
+
+        {__DEV__ ? <ReactPerfButton className={cx(styles.link, styles.devOnly)} /> : null}
       </div>
     </header>
   );
