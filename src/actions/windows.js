a4eee7c7a8b9efc9be9d64f97bf342a230e2677b
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-30 14:25:55 -0500

    start action for opening and closing the start menu

--- src/actions/windows.js
+++ src/actions/windows.js
@@ -1,8 +1,14 @@
-// import {} from '../constants';
-//
-// export function swapWidgetPosition(source, target) {
-//   return (dispatch) => {
-//     dispatch({ type: SWAP_WIDGET_POSITION, source, target });
-//   };
-// }
+import { OPEN_START_MENU, CLOSE_START_MENU} from '../constants';
+
+
+export function toggleStartMenu(source, target) {
+  return (dispatch, getState) => {
+    const { windows: { startMenuOpened } } = getState();
+    if(startMenuOpened) {
+      dispatch({ type: CLOSE_START_MENU });
+    } else {
+      dispatch({ type:OPEN_START_MENU });
+    }
+  };
+}
 
--- src/reducers/windows.js
+++ src/reducers/windows.js
@@ -1,13 +1,16 @@
-import {} from '../constants';
+import {OPEN_START_MENU, CLOSE_START_MENU} from '../constants';
 
 
 const initialState = {
-
+  startMenuOpened: false
 };
 export default function layout(state = initialState, action) {
 
   switch (action.type) {
-
+    case OPEN_START_MENU:
+      return {...state, startMenuOpened: true};
+    case CLOSE_START_MENU:
+      return {...state, startMenuOpened: false};
     default:
       return state;
   }
