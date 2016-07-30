019e47ef47293106e01f3803520524d076d48b62
rmwdeveloper <rmwdeveloper@gmail.com>
2016-07-30 12:19:35 -0500

    time and info styling

--- src/components/WindowsInfoHub/WindowsInfoHub.js
+++ src/components/WindowsInfoHub/WindowsInfoHub.js
@@ -2,9 +2,12 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './WindowsInfoHub.css'; //eslint-disable-line
 
+import computerIcon from './xpComputerXSmall.png';
+
 function WindowsInfoHub({ className }) {
   return (
     <div className={styles.root}>
+      <img className={styles.infoImage} src={computerIcon} width="30" height="29" alt="My Computer Icon for taskbar" />
       <span className={styles.time}>12:03 PM</span>
     </div>
   );
