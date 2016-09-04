9242a025f3325d35fef075d1f904dd8a143bdf10
rmwdeveloper <rmwdeveloper@gmail.com>
2016-09-04 15:00:15 -0500

    Decompose file

---
+++ src/components/Windows/FileWindow/FileWindow.js
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './FileWindow.css'; //eslint-disable-line
+import FileBaseTaskbar from '../FileBaseTaskbar';
+import { windowsClickables } from '../../../constants/windows';
+
+export default function FileWindow(ComposedComponent) {
+  class FileWindow extends Component {
+    static propTypes = {
+      closeFile: PropTypes.func,
+      index: PropTypes.number,
+      toggleWindowMaximize: PropTypes.func,
+      toggleWindowMinimize: PropTypes.func,
+      filename: PropTypes.string,
+      openedFile: PropTypes.object
+    };
+    render() {
+      const { closeFile, index, filename, toggleWindowMaximize, toggleWindowMinimize,
+        openedFile: { maximized, height, width } } = this.props;
+      return (
+        <div style={{height, width}}>
+          <FileBaseTaskbar
+            maximized={maximized}
+            toggleWindowMaximize={toggleWindowMaximize}
+            toggleWindowMinimize={toggleWindowMinimize}
+            index={index}
+            filename={filename}
+            closeFile={closeFile}
+          />
+          <ComposedComponent {...this.state} {...this.props} />
+        </div>);
+    }
+  }
+  return withStyles(styles)(FileWindow);
+}
+
