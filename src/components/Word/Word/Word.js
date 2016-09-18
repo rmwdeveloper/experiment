e7ca712889683d1806e45a2be4b68b9f11845683
rmwdeveloper <rmwdeveloper@gmail.com>
2016-09-17 19:48:08 -0500

    Edit.. word

--- src/components/Word/Word/Word.js
+++ src/components/Word/Word/Word.js
@@ -3,6 +3,8 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Word.css'; //eslint-disable-line
 import { Editor, EditorState, RichUtils } from 'draft-js';
 import WordTaskbar from '../WordTaskbar/WordTaskbar';
+
+
 class Word extends Component {
   constructor(props) {
     super(props);
@@ -11,6 +13,7 @@ class Word extends Component {
     };
     this.onChange = (editorState) => this.setState({ editorState });
     this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    this.onBoldClick = this.onBoldClick.bind(this);
   }
   handleKeyCommand(command) {
     const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
@@ -20,10 +23,13 @@ class Word extends Component {
     }
     return 'not-handled';
   }
+  onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
   render() {
     const { editorState } = this.state;
     return (<div className={styles.root}>
-      <WordTaskbar />
+      <WordTaskbar boldClick={this.onBoldClick} />
       <Editor handleKeyCommand={this.handleKeyCommand} editorState={editorState} onChange={this.onChange} />
     </div>);
   }
