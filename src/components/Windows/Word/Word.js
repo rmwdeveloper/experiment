106469fc3a598b55517b89e34231c3db91cf58e8
rmwdeveloper <rmwdeveloper@gmail.com>
2016-09-17 19:01:11 -0500

    Word.. richutils handle key commands

--- src/components/Windows/Word/Word.js
+++ src/components/Windows/Word/Word.js
@@ -1,7 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Word.css'; //eslint-disable-line
-import {Editor, EditorState} from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 class Word extends Component {
   constructor(props) {
@@ -9,7 +9,16 @@ class Word extends Component {
     this.state = {
       editorState: EditorState.createEmpty()
     };
-    this.onChange = (editorState) => this.setState({editorState});
+    this.onChange = (editorState) => this.setState({ editorState });
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
   }
   render() {
     const { editorState } = this.state;
