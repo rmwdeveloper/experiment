80a80feb7ea1711d7662e682a8113e970c04771e
rmwdeveloper <rmwdeveloper@gmail.com>
2016-09-17 19:52:02 -0500

    Start rich editing util bar

--- src/components/Word/WordTaskbar/WordTaskbar.js
+++ src/components/Word/WordTaskbar/WordTaskbar.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { PropTypes } from  'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './WordTaskbar.css'; //eslint-disable-line
 
 
-function WordTaskbar() {
+function WordTaskbar({boldClick}) {
   return <div>Word Taskbar</div>;
 }
 
+WordTaskbar.propTypes = {
+  boldClick: PropTypes.func.isRequired
+};
 export default withStyles(styles)(WordTaskbar);
