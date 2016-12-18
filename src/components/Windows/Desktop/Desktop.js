af5647d52083bce9631a705da019e936dbc1751d
rmwdeveloper <rmwdeveloper@gmail.com>
2016-12-18 12:59:04 -0500

    Clear actives on start drag select

--- src/components/Windows/Desktop/Desktop.js
+++ src/components/Windows/Desktop/Desktop.js
@@ -121,6 +121,7 @@ class Desktop extends Component {
   }
   startDragSelect(event) {
     const { headerHeight } = this.state;
+    this.props.clearActives();
     this.dragbox = document.getElementById('dragbox');
     if (!this.dragbox) {
       this.dragbox = document.createElement('div');
