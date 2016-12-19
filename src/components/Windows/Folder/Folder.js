2f3fbf8437148097cf011b73291eb1eb968d2e10
rmwdeveloper <rmwdeveloper@gmail.com>
2016-12-19 16:56:00 -0500

    Sidebar styles

--- src/components/Windows/Folder/Folder.js
+++ src/components/Windows/Folder/Folder.js
@@ -4,15 +4,17 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import DesktopItem from '../DesktopItem';
 import FolderSidebar from '../FolderSidebar';
 
-function Folder({openedFile, windowHeight, entities, desktopWidth, desktopHeight, openFile}) {
+function Folder({openedFile, entities, desktopWidth, desktopHeight, openFile}) {
   const folderContents = entities[openedFile.entityId].contents ? entities[openedFile.entityId].contents.map((entityId, index) => {
     return <DesktopItem key={index} desktopWidth={desktopWidth} desktopHeight={desktopHeight} index={index} openFile={openFile} item={entities[entityId]} />;
   }) : null;
-
+  const windowHeight = openedFile.height - 30;
   return (
     <div style={{height: windowHeight}} className={styles.root}>
       <FolderSidebar />
-      {folderContents}
+      <div className={styles.folderContents}>
+        {folderContents}
+      </div>
     </div>
   );
 }
