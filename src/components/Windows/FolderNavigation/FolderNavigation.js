768b6a896d2847a09fb078a4576e18b9077ac09e
rmwdeveloper <rmwdeveloper@gmail.com>
2016-12-24 18:57:50 -0500

    start navigation

--- src/components/Windows/FolderNavigation/FolderNavigation.js
+++ src/components/Windows/FolderNavigation/FolderNavigation.js
@@ -1,14 +1,15 @@
 import React, { PropTypes } from 'react';
 import styles from './FolderNavigation.css'; //eslint-disable-line
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-
+import cx from 'classnames';
 
 function FolderNavigation() {
 
-
   return (
     <div className={styles.root}>
-      folder navigation
+      <i className={cx('fa fa-2x fa-arrow-circle-left')} />
+      <i className={cx('fa fa-2x fa-arrow-circle-right')} />
+      <input type="text"></input>
     </div>
   );
 }
