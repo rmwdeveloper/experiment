9f8c3f09ac9b4406a052c23cae285fb3c178f2db
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-04 15:05:13 -0500

    get rid of a bunch of commented out junk

--- src/components/Windows/Folder/Folder.js
+++ src/components/Windows/Folder/Folder.js
@@ -14,28 +14,6 @@ function Folder({openedFile, selectedDesktopIcons, clearActives, fileSystem, sel
     return <FolderItem moveFile={moveFile} moveFiles={moveFiles} className="folderIcon" parentIndex={openedFile.nodeIndex} selected={selectedIds.includes(desktopitem.index)}
                        key={index} desktopWidth={desktopWidth} desktopHeight={desktopHeight} index={index} openFile={openFile} item={fileSystem[nodeIndex]} />;
   }) : null;
-  // const folderContents = []; // todo refactor this. (Settle this with Desktops folder contents )
-  //
-  // const selectedFileIndices = selectedDesktopIcons.map(iconId => {return parseInt(iconId, 10)});
-  // const unselectedFileIndices = fileSystem[openedFile.nodeIndex].children;
-  // const renderArray = unselectedFileIndices.map(index => {
-  //   return selectedFileIndices.includes(index) ? 'selected' : index;
-  // });
-  // const cleanedRenderArray = renderArray.filter((item, position) => {
-  //   return renderArray.indexOf(item) === position;
-  // });
-  // for (let iterator = 0; iterator < cleanedRenderArray.length; iterator++){
-  //   if (typeof(cleanedRenderArray[iterator]) === 'number') {
-  //     const file = fileSystem[cleanedRenderArray[iterator]];
-  //     file.index = cleanedRenderArray[iterator];
-  //     folderContents.push(<FolderItem className='folderIcon' key={file.index} desktopWidth={desktopWidth} desktopHeight={desktopHeight}
-  //                                         moveFile={moveFile}  openFile={openFile} item={file} />);
-  //   }
-  //   if (cleanedRenderArray[iterator] === 'selected') {
-  //     folderContents.push(<FolderIconsGroup parentIndex={openedFile.index} className='folderIcon'
-  //                                               moveFiles={moveFiles} key={iterator} fileSystem={fileSystem} selectedFileIndices={selectedFileIndices} />);
-  //   }
-  // }
   const windowHeight = openedFile.height - 30;
 
   return (
