2cd737f47ff7fbe3333c80ace1c944b675dfc461
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-08 18:46:50 -0500

    Conv auth to class

--- src/components/Windows/Authenticator/Authenticator.js
+++ src/components/Windows/Authenticator/Authenticator.js
@@ -1,13 +1,12 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import styles from './Authenticator.css'; //eslint-disable-line
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 
-function Authenticator({}) {
-  return <div>Authenticator!!</div>;
+class Authenticator extends Component {
+  render() {
+    return <div>Authenticator!!</div>;
+  }
 }
 
-Authenticator.propTypes = {
-  entityID: PropTypes.number
-};
 export default withStyles(styles)(Authenticator);
