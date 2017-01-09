f9f206512b8f29f39eec99209c8b0702eb9fcce2
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-08 19:18:28 -0500

    Auth Markup

--- src/components/Windows/Authenticator/Authenticator.js
+++ src/components/Windows/Authenticator/Authenticator.js
@@ -4,8 +4,34 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 
 class Authenticator extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mode: 'login'
+    }
+  }
   render() {
-    return <div>Authenticator!!</div>;
+    const { mode } = this.state;
+    return <div className={styles.root}>
+      <form onSubmit={this.handleSubmit}>
+        <div className={styles.controlGroup}>
+          <label for={styles.registerEmail}>Email</label>
+          <input type="email" id={styles.registerEmail} />
+        </div>
+        {
+          mode === 'register' ?
+             <div className={styles.controlGroup}>
+              <label for={styles.registerUsername}>Username</label>
+              <input type="text" id={styles.registerUsername} />
+            </div> : null
+        }
+        <div className={styles.controlGroup}>
+          <label for={styles.registerPassword}>Password</label>
+          <input type="password" id={styles.registerPassword} />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>;
   }
 }
 
