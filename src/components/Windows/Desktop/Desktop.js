4fc8168b0d319fede3f8288403167a0522750e8f
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-10 15:14:59 -0500

    Start Resize action

--- src/components/Windows/Desktop/Desktop.js
+++ src/components/Windows/Desktop/Desktop.js
@@ -11,7 +11,7 @@ import DesktopItem from '../FileIcon';
 import ContextMenu from '../ContextMenu';
 import ErrorWindow from '../ErrorWindow';
 import Evaporate from 'evaporate';
-import { resize_window } from '../../../core/layout'
+import { resizeWindow } from '../../../core/layout'
 
 
 class Desktop extends Component {
@@ -222,7 +222,7 @@ class Desktop extends Component {
     const { itemResized, resizeStartHeight, resizeStartWidth, resizeSideClicked, resizeStartLeft, resizeStartTop } = this.state;
     const deltaX = event.clientX - this.state.resizeStartX;
     const deltaY = event.clientY - this.state.resizeStartY;
-    resize_window(this.resizedItem, itemResized, resizeSideClicked, deltaX, deltaY, resizeStartWidth, resizeStartHeight,
+    resizeWindow(this.resizedItem, resizeSideClicked, deltaX, deltaY, resizeStartWidth, resizeStartHeight,
      resizeStartLeft, resizeStartTop);
     // this.props.resizeFileWindow(itemResized, resizeSideClicked, deltaX, deltaY, resizeStartWidth, resizeStartHeight,
     //   resizeStartLeft, resizeStartTop);
--- src/core/layout.js
+++ src/core/layout.js
@@ -2,6 +2,8 @@ export function sortLayout(a, b) {
   return Number(a.index) - Number(b.index);
 }
 
-export function resizeWindow() {
-  
+export function resizeWindow(resizeNode, sideClicked, deltaX, deltaY, resizeStartWidth, resizeStartHeight,
+  resizeStartLeft, resizeStartTop) {
+  console.log(resizeNode, sideClicked, deltaX, deltaY, resizeStartWidth, resizeStartHeight,
+    resizeStartLeft, resizeStartTop);
 }
