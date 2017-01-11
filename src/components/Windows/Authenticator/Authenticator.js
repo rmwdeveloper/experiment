b8166d60dfd91240bfb5ed711436d5cbe06a8e0c
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-11 15:17:22 -0500

    auth render

--- src/components/Windows/Authenticator/Authenticator.js
+++ src/components/Windows/Authenticator/Authenticator.js
@@ -36,23 +36,38 @@ class Authenticator extends Component {
     xhr.send(JSON.stringify({ email: data.get('email'), username: data.get('username'), password: data.get('password') }));
   }
   render() {
-    const { registering, toggleRegisterMode } = this.props;
-    console.log(this.state.errors);
+    const { registering, toggleRegisterMode } = this.props; // todo: refactor this bulky render.
+    const { errors } = this.state;
+    const usernameErrors = errors.filter(value => { return value.path === 'username'; });
+    const emailErrors = errors.filter(value => { return value.path === 'email'; });
+    const passwordErrors = errors.filter(value => { return value.path === 'password'; });
     const mode = registering ? 'Register' : 'Login';
+    let usernameClassnames = styles.controlGroup;
+    let emailClassnames = styles.controlGroup;
+    let passwordClassnames = styles.controlGroup;
+    if (usernameErrors.length > 0) { usernameClassnames += ` ${styles.controlGroupError}`; }
+    if (emailErrors.length > 0) { emailClassnames += ` ${styles.controlGroupError}`; }
+    if (passwordErrors.length > 0) { passwordClassnames += ` ${styles.controlGroupError}`; }
+
     return <div className={styles.root}>
       <form id={styles.registrationForm} onSubmit={this.handleSubmit}>
-        <div className={styles.controlGroup}>
+        <div className={emailClassnames}>
           <label htmlFor={styles.registerEmail}>Email</label>
+          {
+            emailErrors.map( errorObject => {
+                return <span className={styles.errorMessage}>{errorObject.message}</span>;
+              })
+          }
           <input name="email" type="email" id={styles.registerEmail} />
         </div>
         {
            registering ?
-             <div className={styles.controlGroup}>
+             <div className={usernameClassnames}>
               <label htmlFor={styles.registerUsername}>Username</label>
               <input name="username" type="text" id={styles.registerUsername} />
             </div> : null
         }
-        <div className={styles.controlGroup}>
+        <div className={passwordClassnames}>
           <label htmlFor={styles.registerPassword}>Password</label>
           <input name="password" type="password" id={styles.registerPassword} />
         </div>
