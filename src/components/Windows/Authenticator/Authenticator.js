510593136f80b6b89a8bfe4a53098bfd2f1a06ae
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-11 14:19:20 -0500

    update component

--- src/components/Windows/Authenticator/Authenticator.js
+++ src/components/Windows/Authenticator/Authenticator.js
@@ -3,21 +3,27 @@ import styles from './Authenticator.css'; //eslint-disable-line
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 
-function registerCallback(event) { // todo: Move somewhere lse.
-  const response = event.target;
-  if(response.readyState == XMLHttpRequest.DONE) {
-    if (response.status === 200) {
-      console.log('success');
-    } else {
-      const errors = JSON.parse(response.response);
-
-    }
-  }
-}
+// todo rmw: Abstract away some of the more generic form functionality. Like, POST'ing it to the server, and updating
+// form status. I couldn't find any good generic react form elements.
 class Authenticator extends Component {
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.registrationCallback = this.registrationCallback.bind(this);
+    this.state = {
+      errors: []
+    }
+  }
+  registrationCallback() {
+    const response = event.target;
+    if(response.readyState == XMLHttpRequest.DONE) {
+      if (response.status === 200) {
+        console.log('success');
+      } else {
+        const errors = JSON.parse(response.response);
+        this.setState({errors});
+      }
+    }
   }
   handleSubmit(event) {
     event.preventDefault();
@@ -25,12 +31,13 @@ class Authenticator extends Component {
     const data = new FormData(form);
     const xhr = new XMLHttpRequest();
     xhr.open('POST', '/register', true);
-    xhr.onreadystatechange = registerCallback;
+    xhr.onreadystatechange = this.registrationCallback;
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify({ email: data.get('email'), username: data.get('username'), password: data.get('password') }));
   }
   render() {
     const { registering, toggleRegisterMode } = this.props;
+    console.log(this.state.errors);
     const mode = registering ? 'Register' : 'Login';
     return <div className={styles.root}>
       <form id={styles.registrationForm} onSubmit={this.handleSubmit}>
