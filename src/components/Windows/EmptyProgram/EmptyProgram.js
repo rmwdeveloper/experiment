8be1ff3ee6afa1c6472ecccc5d8b4ad0b38dd4f6
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-14 16:41:21 -0500

    interation

--- src/components/Windows/EmptyProgram/EmptyProgram.js
+++ src/components/Windows/EmptyProgram/EmptyProgram.js
@@ -5,7 +5,15 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 
 function EmptyProgram({}) {
-  return <div>Empty Program</div>;
+  return <div className={styles.root}>
+    <p>A problem has been detected and this program cannot start.</p>
+    <p>PROGRAM NOT IMPLEMENTED</p>
+    <p>Technical Information:</p>
+    <p>*** STOP: 0x000000ED (0X80F128D0, 0xc000009c, 0X0000000, 0x00000000)</p>
+    <p>*** STOP: 0X80F128D0 (0X80F128D0, 0X80F128D0, 0xc000009c, 0x00000000)</p>
+    <p>*** STOP: 0x000000ED (0X80F128D0, 0xc000009c, 0X80F128D0, 0x00000000)</p>
+    <p>*** STOP: 0x000000ED (0X80F128D0, 0xc000009c, 0X0000000, 0X80F128D0)</p>
+  </div>;
 }
 
 EmptyProgram.propTypes = {
