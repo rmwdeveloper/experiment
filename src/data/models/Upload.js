92e180a8dce979e3c7369784b079e72489cac46a
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-15 16:47:24 -0500

    uploads

---
+++ src/data/models/Upload.js
@@ -0,0 +1,23 @@
+import DataType from 'sequelize';
+import Model from '../sequelize';
+
+const Upload = Model.define('Upload', {
+  id: {
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    validate: {isInt: true, notNull: true, min: 1}
+  },
+  location: {
+    type: DataType.STRING
+  },
+  extension: {
+    type: DataType.STRING
+  },
+  fileSize: {
+    type: DataType.BIGINT
+  },
+
+});
+
+export default Upload;
