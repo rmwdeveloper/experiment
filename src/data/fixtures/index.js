bca946d7d1b4cca1a8d98d162eac6661ad06afc7
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-23 13:34:56 -0500

    Include FileNodeId in the uploads

--- src/data/fixtures/index.js
+++ src/data/fixtures/index.js
@@ -2,7 +2,7 @@ import fileNodes from './fileNodes';
 import fileNodeMetadata from './fileNodeMetadata';
 import fileSystem from './fileSystem';
 
-const fileNodesFixture = fileNodes.map( node => { const {name, permissions, nodeIndex, extension } = node.data; return {name, permissions, nodeIndex, extension }});
+const fileNodesFixture = fileNodes.map( node => { const {name, permissions, nodeIndex, extension, FileNodeId } = node.data; return {name, permissions, nodeIndex, extension, FileNodeId }});
 const fileNodesMetadataFixture = fileNodeMetadata.map( node => { const {name, value, nodeIndex } = node.data; return {name, value, nodeIndex }});
 
 export { fileNodesFixture, fileNodesMetadataFixture, fileSystem };
