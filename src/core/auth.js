7ab341a0c660910c43c1833324b869899ede2e8f
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-24 13:07:25 -0500

    Slight edit

--- src/core/auth.js
+++ src/core/auth.js
@@ -1,9 +1,8 @@
-import { User } from '../data/models';
+import { User, FileSystem, FileNode, FileNodeMetadata } from '../data/models';
 
 
-export function getUser(username = null, id = null) {
-  
-  return User.findOne({ where: { username  }, attributes: ['username', 'email', 'emailConfirmed'],
+export function getUser(username) {
+  return User.findOne({ where: { username }, attributes: ['username', 'email', 'emailConfirmed'],
     include: [{ model: FileSystem, attributes: ['diskSpace'],
     include: [{ model: FileNode, attributes: ['name', 'permissions', 'extension', 'nodeIndex', 'FileNodeId'],
     include: [{ model: FileNodeMetadata, attributes: ['name', 'value'] }]
