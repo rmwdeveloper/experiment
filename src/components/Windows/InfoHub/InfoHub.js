a535b2b4da6faa2015f08cf14af6fe593c6a0193
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-25 14:36:43 -0500

    styles

--- src/components/Windows/InfoHub/InfoHub.js
+++ src/components/Windows/InfoHub/InfoHub.js
@@ -1,15 +1,16 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './InfoHub.css'; //eslint-disable-line
-
+import cx from 'classnames';
 import computerIcon from './xpComputerXSmall.png'; //eslint-disable-line
 import SimpleTimer from '../../SimpleTimer'; //eslint-disable-line
 
 function InfoHub({ className }) {
   return (
     <div className={styles.root}>
-      <img className={styles.infoImage} src={computerIcon} width="30" height="29" alt="My Computer Icon for taskbar" />
-      <img className={styles.infoImage} src={computerIcon} width="30" height="29" alt="My Computer Icon for taskbar" />
+      <i className={cx(styles.infoImage, 'fa fa-database')} width="30" height="29" alt="Space Available Indicator" />
+      <i className={cx(styles.infoImage, 'fa fa-bullseye')} width="30" height="29" alt="Space Available Indicator" />
+      {/*<img className={styles.infoImage} src={computerIcon} width="30" height="29" alt="My Computer Icon for taskbar" /> */}
       <SimpleTimer />
     </div>
   );
