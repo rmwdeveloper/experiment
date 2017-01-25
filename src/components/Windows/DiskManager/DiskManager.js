ef94fe9a16e5148ccf43b4a09486dc354f6e210b
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-25 16:00:39 -0500

    fixes

--- src/components/Windows/DiskManager/DiskManager.js
+++ src/components/Windows/DiskManager/DiskManager.js
@@ -8,11 +8,37 @@ class DiskManager extends Component {
   };
   constructor() {
     super();
+    this.drawChart = this.drawChart.bind(this);
+  }
+  drawChart() {
+    // Create the data table.
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    const options = {'title':'How Much Pizza I Ate Last Night',
+      'width':400,
+      'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    const chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
+  componentDidMount() {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(this.drawChart);
   }
   render() {
     return <div className={styles.root}>
-      {diskSpace} {usedSpace}
-      <div className={styles.charts}>
+      <div id="chart_div" className={styles.charts}>
 
       </div>
     </div>;
