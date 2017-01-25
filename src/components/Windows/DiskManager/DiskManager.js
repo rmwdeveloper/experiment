4b2ae5a68f51d4b4714d4e4a1560fbdc4d852721
rmwdeveloper <rmwdeveloper@gmail.com>
2017-01-25 15:55:44 -0500

    Conv Disk Manager to class

--- src/components/Windows/DiskManager/DiskManager.js
+++ src/components/Windows/DiskManager/DiskManager.js
@@ -1,20 +1,23 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import styles from './DiskManager.css'; //eslint-disable-line
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
+class DiskManager extends Component {
+  static propTypes = {
 
+  };
+  constructor() {
+    super();
+  }
+  render() {
+    return <div className={styles.root}>
+      {diskSpace} {usedSpace}
+      <div className={styles.charts}>
 
-function DiskManager({diskSpace, usedSpace, user}) {
-  return <div className={styles.root}>
-    {diskSpace} {usedSpace}
-    <div className={styles.spaceAvailable}>
-      <span> {diskSpace} {usedSpace}</span>
-      <span>Total Space Available: {diskSpace - usedSpace}</span>
-    </div>
-  </div>;
+      </div>
+    </div>;
+  }
 }
 
-DiskManager.propTypes = {
-  entityID: PropTypes.number
-};
+
 export default withStyles(styles)(DiskManager);
