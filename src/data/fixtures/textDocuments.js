ca6a74d2b22777b126738acafe5a23a6d1957665
rmwdeveloper <rmwdeveloper@gmail.com>
2017-02-07 13:17:32 -0500

    TextDocument fixture for User object

---
+++ src/data/fixtures/textDocuments.js
@@ -0,0 +1,12 @@
+const todoList = require('../textDocuments/todoList');
+
+
+module.exports = [
+  {
+    "model": "TextDocument",
+    "data": {
+      "UserId": 1,
+      "markup": todoList,
+    }
+  }
+];
