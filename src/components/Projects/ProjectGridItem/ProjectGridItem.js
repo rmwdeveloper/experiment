af9d964c7aa78740f860fbbd43f9e8898209ecc6
rmwdeveloper <rmwdeveloper@gmail.com>
2017-02-27 18:34:20 -0500

    current iter

--- src/components/Projects/ProjectGridItem/ProjectGridItem.js
+++ src/components/Projects/ProjectGridItem/ProjectGridItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './ProjectGridItem.css'; //eslint-disable-line
+import { TweenLite } from 'gsap';
+
+
 
 function ProjectGridItem({ project: {name, technologies, role, description, link}}) {
-
   return (
     <div className={styles.root}>
       {name}
