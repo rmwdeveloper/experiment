d915bc3441f90f549a6a5cc42f0cd2ac8c616c5a
rmwdeveloper <rmwdeveloper@gmail.com>
2017-02-28 14:06:40 -0500

    current iter

--- src/components/Projects/Carousel/Carousel.js
+++ src/components/Projects/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes, Component} from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Carousel.css'; //eslint-disable-line
 import cx from 'classnames';
@@ -6,6 +6,39 @@ import { TweenLite } from 'gsap';
 
 
 class Carousel extends Component {
+  constructor() {
+    super();
+    this.prev = this.prev.bind(this);
+    this.next = this.next.bind(this);
+    this.applyProps = this.applyProps.bind(this);
+  }
+  applyProps(props) {
+    const { slide, tweenProps: {y} } = props;
+
+     slide.style.transform = `rotateY( ${y}deg ) translateZ(288px)`;
+
+    // console.log(slide.style.transform);
+  }
+  prev() {
+    const slides = document.querySelectorAll(`.${styles.slide}`);
+    for ( let iterator = 0; iterator < slides.length; iterator++){
+      const slide = slides[iterator];
+      const y = slide.getAttribute('y');
+      const tweenProps = {y};
+      TweenLite.to(tweenProps, 1, {y: parseInt(y, 10) + 90 , onUpdate:this.applyProps, onUpdateParams: [{slide, tweenProps}]});
+      slide.setAttribute('y', parseInt(y, 10) + 90);
+    }
+  }
+  next() {
+    const slides = document.querySelectorAll(`.${styles.slide}`);
+    for ( let iterator = 0; iterator < slides.length; iterator++){
+      const slide = slides[iterator];
+      const y = slide.getAttribute('y');
+      const tweenProps = {y};
+      TweenLite.to(tweenProps, 1, {y: parseInt(y, 10) - 90 , onUpdate:this.applyProps, onUpdateParams: [{slide, tweenProps}]});
+      slide.setAttribute('y', parseInt(y, 10) - 90);
+    }
+  }
   render() {
     const { allProjects } = this.props;
 
@@ -16,12 +49,12 @@ class Carousel extends Component {
           {allProjects ? allProjects.map( (project, index) => {
             const style = {};
             style.transform = `rotateY(  ${index * rotation}deg ) translateZ( 288px)`;
-            return <div style={style} key={index} className={styles.slide}>{index}</div>;
+            return <div y={index * rotation} style={style} key={index} className={styles.slide}>{index}</div>;
           } ): null}
         </div>
       </div>
-      <i className={cx(styles.control, styles.left, 'fa fa-chevron-left')} />
-      <i className={cx(styles.control, styles.right, 'fa fa-chevron-right')} />
+      <i onClick={this.prev} className={cx(styles.control, styles.left, 'fa fa-chevron-left')} />
+      <i onClick={this.next} className={cx(styles.control, styles.right, 'fa fa-chevron-right')} />
     </div>);
   }
 }
