b49e090bad41ffd3eb4368c126850a2eecbdd12d
rmwdeveloper <rmwdeveloper@gmail.com>
2017-02-28 11:47:06 -0500

    current iter

--- src/components/Projects/Carousel/Carousel.js
+++ src/components/Projects/Carousel/Carousel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Carousel.css'; //eslint-disable-line
+import cx from 'classnames';
 import { TweenLite } from 'gsap';
 
 
@@ -9,16 +10,16 @@ function Carousel({allProjects}) {
   const rotation = 360 / allProjects.length;
   return (<div id={styles.root}>
     <div id={styles.container}>
-      <div id={styles.carousel}>
+      <div id={styles.slidesContainer}>
         {allProjects ? allProjects.map( (project, index) => {
-
           const style = {};
           style.transform = `rotateY(  ${index * rotation}deg ) translateZ( 288px)`;
-          console.log(style);
           return <div style={style} key={index} className={styles.slide}>{index}</div>;
         } ): null}
       </div>
     </div>
+    <i className={cx(styles.control, styles.left, 'fa fa-chevron-left')} />
+    <i className={cx(styles.control, styles.right, 'fa fa-chevron-right')} />
   </div>);
 }
 
