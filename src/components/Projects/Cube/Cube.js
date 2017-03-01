634c15651a423585e0a2d85e9be43b74f6d59f6a
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-01 13:46:20 -0500

    current iteration

--- src/components/Projects/Cube/Cube.js
+++ src/components/Projects/Cube/Cube.js
@@ -14,6 +14,19 @@ class Cube extends Component {
       return <div key={index} className={cx(styles[side], styles.side)}> {side} </div>
     });
   }
+  componentDidMount() {
+    this.cube = document.getElementById(styles.sidesContainer);
+    this.rotate = new TimelineMax();
+    // this.horizontalRotate = new TimelineMax();
+    // this.verticalRotate = new TimelineMax();
+
+    this.rotate.to(this.cube, 16, {transform: 'rotate3d(8, 1, 1, 180deg)', repeatDelay: 0, repeat: -1, yoyo: true});
+    // this.horizontalRotate.to(this.cube, 4, {transform: 'rotateY(180deg)', repeatDelay: 0, repeat: -1, yoyo: true});
+    // this.verticalRotate.to(this.cube, 4, {transform: 'rotateX(180deg)', repeatDelay: 0, repeat: -1, yoyo: true});
+    // this.rotate.play();
+    // this.horizontalRotate.play();
+    // this.verticalRotate.play();
+  }
   render() {
     const { allProjects } = this.props;
     const sides = this.renderSides();
