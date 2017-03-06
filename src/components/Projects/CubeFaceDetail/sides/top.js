b8838ce3e560b9e6a4d0cf305568d7036191c845
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-06 18:49:44 -0500

    bugfix

--- src/components/Projects/CubeFaceDetail/sides/top.js
+++ src/components/Projects/CubeFaceDetail/sides/top.js
@@ -3,7 +3,5 @@ import cx from 'classnames';
 
 
 export default function Top({style, url, image, network}) {
-  const iconClasses = cx(style.socialNetwork, {'inactive': !url });
-
   return <div>Top</div>;
 }
