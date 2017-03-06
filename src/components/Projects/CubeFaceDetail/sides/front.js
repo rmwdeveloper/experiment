b6039ca9b49528cdb80ff61d4a06c8fc7b95f448
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-06 18:56:25 -0500

    current iteration

--- src/components/Projects/CubeFaceDetail/sides/front.js
+++ src/components/Projects/CubeFaceDetail/sides/front.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import cx from 'classnames';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import styles from './front.css'; //eslint-disable-line
 
+function Front({}) {
 
-export default function Front({}) {
-
-  return <div>front</div>;
+  return <div className={styles.root}>front</div>;
 }
+
+
+export default withStyles(styles)(Front);
