08ee64539635d85eaf4d829fe13676269d8fc4a6
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-07 14:39:16 -0500

    boner

--- src/components/Projects/CubeFaceDetail/sides/left.js
+++ src/components/Projects/CubeFaceDetail/sides/left.js
@@ -4,5 +4,11 @@ import cx from 'classnames';
 
 export default function left({}) {
 
-  return <div>left</div>;
+  return <div> Hello,I'm a Web Developer urrently residing in Rockaway, New Jersey. I spend my days doing freelancing ,
+    dabbling in all kinds of different web technologies. I'm familiar with both front end(HTML5, CSS3, Javascript,
+    jQuery) and back end(Python Django, PHP) programming. I strive to create useable and visually appealing websites.
+
+    I enjoy all aspects of web development. I'm currently looking for full time employment and more freelancing gigs.
+    Aside from web development, I enjoy fitness, the outdoors, reading, and TV.
+  </div>;
 }
