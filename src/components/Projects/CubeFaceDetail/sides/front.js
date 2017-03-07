2b6dc12298420a0c8df3070c7cec86db6fb734b9
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-07 13:35:31 -0500

    current iteration

--- src/components/Projects/CubeFaceDetail/sides/front.js
+++ src/components/Projects/CubeFaceDetail/sides/front.js
@@ -4,15 +4,28 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './front.css'; //eslint-disable-line
 
 function Front({professionalProjects}) {
-  console.log(professionalProjects);
+
   return <div className={styles.root}>
     <h1 className={styles.title}>Lorem Ipsum</h1>
     <div className={styles.projectsGrid}>
       { professionalProjects.map(project => {
-        const { description, images, link, name, role } = project;
-
+        const { description, images, link, name, role, technologies } = project;
         return (<div className={styles.projectRow}>
-          <h3>{name}</h3>
+          <h3 className={styles.projectName}>{name}</h3>
+          <div className={styles.contentBlock}>
+            <div className={styles.imageBlock}>
+              {images ? images.map(image => {
+                return <img src={image} />;
+            }) : null }
+            </div>
+            <div className={styles.detailBlock}>
+              <p>{role}</p>
+              <p>{description}</p>
+              {technologies.map(technology => {
+                return <span> {technology} </span>;
+              })}
+            </div>
+          </div>
         </div>);
       })}
     </div>
