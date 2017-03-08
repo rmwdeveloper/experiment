4d15c3e78646271a2fb39b554b38f38383b50d85
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-08 15:48:28 -0500

    Text documents, add fixtures

--- src/data/fixtures/textDocuments.js
+++ src/data/fixtures/textDocuments.js
@@ -1,4 +1,7 @@
 const todoList = require('../textDocuments/todoList');
+const directions = require('../textDocuments/directions');
+const faq = require('../textDocuments/faq');
+const resume = require('../textDocuments/resume');
 
 
 module.exports = [
@@ -6,8 +9,32 @@ module.exports = [
     "model": "TextDocument",
     "data": {
       "UserId": 1,
+      "markup": faq,
+      "FileNodeId": 25
+    }
+  },
+  {
+    "model": "TextDocument",
+    "data": {
+      "UserId": 1,
       "markup": todoList,
       "FileNodeId": 28
     }
-  }
+  },
+  {
+    "model": "TextDocument",
+    "data": {
+      "UserId": 1,
+      "markup": resume,
+      "FileNodeId": 30
+    }
+  },{
+    "model": "TextDocument",
+    "data": {
+      "UserId": 1,
+      "markup": directions,
+      "FileNodeId": 31
+    }
+  },
+
 ];
