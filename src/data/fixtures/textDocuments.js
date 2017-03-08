6882b4e205d66e95a2d9187c3ea9284cd16d0d7d
rmwdeveloper <rmwdeveloper@gmail.com>
2017-03-08 15:54:07 -0500

    remove UserID from fixtures

--- src/data/fixtures/textDocuments.js
+++ src/data/fixtures/textDocuments.js
@@ -8,7 +8,6 @@ module.exports = [
   {
     "model": "TextDocument",
     "data": {
-      "UserId": 1,
       "markup": faq,
       "FileNodeId": 25
     }
@@ -16,7 +15,6 @@ module.exports = [
   {
     "model": "TextDocument",
     "data": {
-      "UserId": 1,
       "markup": todoList,
       "FileNodeId": 28
     }
@@ -24,14 +22,12 @@ module.exports = [
   {
     "model": "TextDocument",
     "data": {
-      "UserId": 1,
       "markup": resume,
       "FileNodeId": 30
     }
   },{
     "model": "TextDocument",
     "data": {
-      "UserId": 1,
       "markup": directions,
       "FileNodeId": 31
     }
